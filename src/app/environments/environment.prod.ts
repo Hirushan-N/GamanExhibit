344a55acdc26870d87abd9071154dcfc212415c4
Hirushan-N <78243763+Hirushan-N@users.noreply.github.com>
2025-01-04 00:02:33 +0530

    update api url

---
+++ src/app/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = 
+{ 
+    production: true, 
+    apiBaseUrl: 'https://gamanapi-a3658577c337.herokuapp.com/api' 
+};
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
-  private apiBaseUrl = 'http://localhost:3000/api/users';
+  private apiBaseUrl = `${environment.apiBaseUrl}/users`;
 
   constructor(private http: HttpClient) {}
 
